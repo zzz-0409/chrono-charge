83432c9f59ff03e5b5547434279527830daaa2e8
zzz... <tsryt0409@gmail.com>
2026-05-26 11:26:54 +0900

    Lift drive card meta text

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_VERSION = "chrono-drive-pwa-20260526-card-zoom-ruby";
+const CACHE_VERSION = "chrono-drive-pwa-20260526-drive-meta-lift";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
 const APP_SHELL_URLS = [
   "/",
   "/index.html",
-  "/styles.css?v=20260526cardzoomruby",
+  "/styles.css?v=20260526driveMetaLift",
   "/manifest.webmanifest",
   "/assets/icons/icon-192.png",
   "/assets/icons/icon-512.png",
