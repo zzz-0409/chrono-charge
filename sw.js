b39d1cac6ccb1925d21a1306e177e782a511dcd7
zzz... <tsryt0409@gmail.com>
2026-05-27 21:24:40 +0900

    Refine bottom nav selected styling

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_VERSION = "chrono-drive-pwa-20260527-nav-fix";
+const CACHE_VERSION = "chrono-drive-pwa-20260527-nav-color";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
 const APP_SHELL_URLS = [
   "/",
   "/index.html",
-  "/styles.css?v=20260527navfix",
+  "/styles.css?v=20260527navcolor",
   "/manifest.webmanifest",
   "/assets/icons/icon-192.png",
   "/assets/icons/icon-512.png",
