f342924102a07139242514b7be0d4d18cc3ad8aa
zzz... <tsryt0409@gmail.com>
2026-05-27 12:05:23 +0900

    Adjust opponent board layout

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_VERSION = "chrono-drive-pwa-20260527-thumbs";
+const CACHE_VERSION = "chrono-drive-pwa-20260527-opponent-layout";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
 const APP_SHELL_URLS = [
   "/",
   "/index.html",
-  "/styles.css?v=20260526bottomHomeButton",
+  "/styles.css?v=20260527opponentLayout",
   "/manifest.webmanifest",
   "/assets/icons/icon-192.png",
   "/assets/icons/icon-512.png",
